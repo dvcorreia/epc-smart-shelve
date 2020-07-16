e1926105ea7dc26fb979f2d12c37bb9f07be40c0
dvcorreia <dv_correia@hotmail.com>
2020-07-16 15:11:30 +0100

    work on services + tools folder

---
+++ tools/get_tools.sh
@@ -0,0 +1,12 @@
+wget https://oss.sonatype.org/content/repositories/public/org/fosstrak/fc/fc-client/1.2.0/fc-client-1.2.0-bin-with-dependencies.zip
+unzip fc-client-1.2.0-bin-with-dependencies.zip
+rm fc-client-1.2.0-bin-with-dependencies.zip
+
+
+wget https://oss.sonatype.org/content/repositories/public/org/fosstrak/epcis/epcis-captureclient/0.5.0/epcis-captureclient-0.5.0-bin-with-dependencies.zip
+unzip epcis-captureclient-0.5.0-bin-with-dependencies.zip
+rm epcis-captureclient-0.5.0-bin-with-dependencies.zip
+
+wget https://oss.sonatype.org/content/repositories/public/org/fosstrak/epcis/epcis-queryclient/0.5.0/epcis-queryclient-0.5.0-bin-with-dependencies.zip
+unzip epcis-queryclient-0.5.0-bin-with-dependencies.zip
+rm epcis-queryclient-0.5.0-bin-with-dependencies.zip
