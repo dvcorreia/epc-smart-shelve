f72a63da4afc29c137bcd14ba1d753bbb986f081
dvcorreia <dv_correia@hotmail.com>
2020-07-22 18:40:36 +0100

    reset script for platform

---
+++ services/reset.sh
@@ -0,0 +1,13 @@
+echo "Compose down ..."
+docker-compose down
+echo "Deleting log data ..."
+rm -rf ./**/logs
+echo "Deleting database data ..."
+rm -rf epcis-db/data/*
+echo "Prune containers ..."
+docker container prune --force
+echo "Remove cache images ..."
+docker image remove services_middleware services_capture
+echo "Deleting tcp dump data ..."
+rm -rf tcpdump/*
+echo "Done"
