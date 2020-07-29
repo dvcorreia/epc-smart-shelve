43f9f1483852cd1980184068fa1fd8485849da71
dvcorreia <dv_correia@hotmail.com>
2020-07-29 17:36:25 +0100

    provisory configs and files for testing and dev of middleware and capture application

--- services/reset.sh
+++ services/reset.sh
@@ -1,13 +1,19 @@
 echo "Compose down ..."
 docker-compose down
+
 echo "Deleting log data ..."
 rm -rf ./**/logs
-echo "Deleting database data ..."
-rm -rf epcis-db/data/*
+
+#echo "Deleting database data ..."
+#rm -rf epcis-db/data/*
+
 echo "Prune containers ..."
 docker container prune --force
-echo "Remove cache images ..."
-docker image remove services_middleware services_capture
+
+echo "Remove cache images that generate problems ..."
+docker image remove services_middleware
+
 echo "Deleting tcp dump data ..."
 rm -rf tcpdump/*
+
 echo "Done"
